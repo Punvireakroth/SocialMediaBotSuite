8a14871f428e81f1612857a673d0145f49ff0721
Pun VireakRoth <vireakrothpun@gmail.com>
2024-05-13 05:30:33 +0300

    Personalize sentiment messages

--- utils/translations.js
+++ utils/translations.js
@@ -2,7 +2,9 @@
 
 const translations = {
     en: {
-        thankYouMessage: 'Thank you for your comment! Please check your inbox. 📨',
+        thankYouMessage: `Delighted to hear you love our product - we're here to help you get the most out of it! 😍`,
+        complaintResponse: `We apologize for any inconvenience you've experienced. Our team will look into this matter promptly. If you need further assistance, please don't hesitate to reach out to us directly. 🙏`,
+        interestResponse: `We're glad to hear that you're interested in our product! We will send you a direct message if you have any questions or would like more information. 😇`
     },
     kh: {
         thankYouMessage: 'អរគុណចំពោះការខមិនរបស់អ្នក!😊 សូមពិនិត្យមើលប្រអប់សាររបស់អ្នក។ 📨'

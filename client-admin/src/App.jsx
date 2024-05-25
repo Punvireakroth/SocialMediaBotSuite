8535f5ea2ba155617a9a5bd76dda441c7f31e32b
Pun VireakRoth <vireakrothpun@gmail.com>
2024-05-25 16:55:44 +0300

    Page structure and Routes

--- client-admin/src/App.jsx
+++ client-admin/src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+
 import KeywordForm from './components/KeywordForm';
 import KeywordList from './components/KeywordList';
 
 function App() {
   return (
-    <div className="App">
-      <KeywordForm />
-      <KeywordList />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow container mx-auto p-4">
+          <Routes>
+            <Route path="/add-keyword" element={<KeywordForm />} />
+            <Route path="/keyword-list" element={<KeywordList />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ client-admin/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <footer className="bg-gray-800 p-4 text-white mt-8">
+            <div className="container mx-auto text-center">
+                <h1>Welcome, good day!</h1>
+            </div>
+        </footer>
+    );
+};
+
+export default Home;
